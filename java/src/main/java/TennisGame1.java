f278401d5a0bc1d12be7836341482b654f867a0e
KevinOspina <kevinospina03@gmail.com>
2020-10-19 16:56:09 -0500

    refactoring game1

---
+++ java/src/main/java/TennisGame1.java
@@ -0,0 +1,109 @@
+public class TennisGame1 implements TennisGame {
+    
+    private int m_score1 = 0;
+    private int m_score2 = 0;
+    private String player1Name;
+    private String player2Name;
+    String score = "";
+
+    public TennisGame1(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    public void wonPoint(String playerName) {
+        if (playerName == "player1")
+            m_score1 += 1;
+        else
+            m_score2 += 1;
+    }
+
+    
+    public void draft() {
+    	 switch (m_score1){
+             case 0:
+                     score = "Love-All";
+                 break;
+             case 1:
+                     score = "Fifteen-All";
+                 break;
+             case 2:
+                     score = "Thirty-All";
+                 break;
+             default:
+                     score = "Deuce";
+                 break;
+             
+         }
+    }
+    
+    
+    public void tempScore() {
+    	int minusResult = m_score1-m_score2;
+    	
+    	if (minusResult==1) { 
+    		score = "Advantage player1";
+    	}
+    	 if (minusResult==1) {
+    		 score ="Advantage player1";
+    	}
+         else if (minusResult ==-1) { 
+        	 score ="Advantage player2";
+        }
+         else if (minusResult>=2) {
+        	 score = "Win for player1";
+        }
+         else { 
+        	 score ="Win for player2";
+         }
+    	
+    }
+    
+    public void roundScore() {
+    	int temp_Score;
+    	
+    	for (int i=1; i<3; i++)
+    	{
+    		if(i==1) {
+    			temp_Score = m_score1;
+    		}
+    		else {
+    			score+="-";
+    			temp_Score = m_score2;
+    		}
+    		tempScore(temp_Score)
+    	}
+    }
+    
+    public void tempScore(int temp_Score) {
+        switch(temp_Score){
+            case 0:
+                score+="Love";
+                break;
+            case 1:
+                score+="Fifteen";
+                break;
+            case 2:
+                score+="Thirty";
+                break;
+            case 3:
+                score+="Forty";
+                break;	
+    	}
+    }
+    
+    
+    public String getScore() {
+      
+    	if(m_score1 == m_score2) {
+    		draft();
+    	}
+    	else if(m_score1 >= 4 ||  m_score2 >=4)
+    	{
+    		tempScore();
+    	}
+    	else {
+    		return score;
+    	}
+    }
+}
